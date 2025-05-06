d3e675036c1c890c539961081af025ed51837a3b
lamb <114350950+Lambangramadhian@users.noreply.github.com>
2025-05-06 11:07:13 +0700

    Enhance logging in make_prediction function by adding process name; improve clarity in log entries for predictions and cache hits

--- predict.py
+++ predict.py
@@ -5,10 +5,13 @@ import joblib
 import hashlib
 import datetime
 from flask import current_app
+from multiprocessing import current_process
 
+# Load model dan vectorizer
 model = joblib.load(os.getenv("MODEL_PATH", "model/random_forest_web_ids.pkl"))
 vectorizer = joblib.load(os.getenv("VECTORIZER_PATH", "model/tfidf_vectorizer.pkl"))
 
+# Inisialisasi koneksi Redis
 redis_client = redis.StrictRedis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
@@ -16,10 +19,8 @@ redis_client = redis.StrictRedis(
     decode_responses=True
 )
 
-
 def get_cache_key(input_text: str) -> str:
     return f"prediction:{hashlib.sha256(input_text.encode()).hexdigest()}"
-
 
 def flatten_dict(d, parent_key='', sep='||'):
     items = []
@@ -35,6 +36,7 @@ def make_prediction(method: str, url: str, body, client_ip: str = "Tidak Diketah
     try:
         now = datetime.datetime.now()
         timestamp_str = now.strftime("[%d/%m/%Y %H:%M:%S]")
+        process_name = current_process().name
 
         if isinstance(body, dict):
             flat_body = flatten_dict(body)
@@ -53,7 +55,9 @@ def make_prediction(method: str, url: str, body, client_ip: str = "Tidak Diketah
         }
         formatted_input = json.dumps(structured_log, indent=4, ensure_ascii=False)
 
-        current_app.logger.info(f"{timestamp_str} IP: {client_ip} | User-ID: {user_id} | Input: {formatted_input}")
+        current_app.logger.info(
+            f"{timestamp_str} Worker: {process_name} | IP: {client_ip} | User-ID: {user_id} | Input: {formatted_input}"
+        )
 
         if not input_text:
             return {"error": "Payload diperlukan"}
@@ -61,7 +65,7 @@ def make_prediction(method: str, url: str, body, client_ip: str = "Tidak Diketah
         cache_key = get_cache_key(input_text)
         hasil_cache = redis_client.get(cache_key)
         if hasil_cache:
-            current_app.logger.info(f"{timestamp_str} Prediksi (cache): {hasil_cache}")
+            current_app.logger.info(f"{timestamp_str} Worker: {process_name} | Prediksi (cache): {hasil_cache}")
             return {"prediction": hasil_cache}
 
         input_vector = vectorizer.transform([input_text])
@@ -72,7 +76,7 @@ def make_prediction(method: str, url: str, body, client_ip: str = "Tidak Diketah
 
         redis_client.setex(cache_key, 60, label)
 
-        current_app.logger.info(f"{timestamp_str} Prediksi: {label}")
+        current_app.logger.info(f"{timestamp_str} Worker: {process_name} | Prediksi: {label}")
         return {"prediction": label}
 
     except Exception as e:
